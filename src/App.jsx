6e1ea4351ad852b517ae6c792e1b909e7135697e
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-11-16 17:30:27 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {Routes, Route, useNavigate, Navigate } from "react-router-dom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeSection, setActiveSection] = useState(0);
+  const navigate = useNavigate();
+
+  const SECTIONS = [
+    { id: 0, name: "Home" , path: "/"},
+    { id: 1, name: "About", path: "/about" },
+    { id: 2, name: "Details", path: "/details" },
+    { id: 3, name: "Contact us" , path: "/contact-us"},
+  ];
+
+  const handleClickSection = (sectionId, path) => {
+    setActiveSection(sectionId);
+    navigate(path);
+  }
 
   return (
-  <>
-  <div className="navbar">
-    <div className='navbar-item'>Home</div>
-    <div className='navbar-item'>About </div>
-    <div className='navbar-item'>Details</div>
-    <div className='navbar-item'>Contact us</div>
-  </div>
-  </> 
-  )
+    <>
+      <div className="navbar">
+        {SECTIONS.map((section) => (
+          <div key={section.id} className={`navbar-item ${activeSection == section.id ? `active` : ''}`}
+           onClick={()=> handleClickSection(section.id, section.path)}>
+            {section.name}
+          </div>
+        ))}
+      </div>
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/details" element={<h1>Details</h1>} />
+        <Route path="/contact-us" element={<h1>Contact us</h1>} />
+        <Route path="*" element={<Navigate to="/" /> } />
+      </Routes>
+    </>
+  );
 }
-
-export default App
+export default App;
